a81377c4136dc168dbb793e84e003abe80de2c70
Tahir. I <69819070+tismailw@users.noreply.github.com>
2024-02-28 01:22:37 -0500

    Update tmbd.py
    finished the tmbd class on getting x amount of people and the retrospective amount of movies they were in. Made a scatter plot off of it.

--- tmbd.py
+++ tmbd.py
@@ -1,6 +1,6 @@
 import tmdbsimple as tmdb
 import requests
-import json
+import matplotlib.pyplot as plt
 
 #api key
 tmdb.API_KEY = 'e4194ed7bb70d235c3f504a4165912d3'
@@ -21,17 +21,45 @@ response = requests.get(url, headers=headers)
 data = response.json()
 
 names_list = []
+ids_list = []
 
 # Iterate over the items in the 'results' list
 for item in data['results']:
     # Extract the name of each person and append it to the names_list
     names_list.append(item['name'])
+    ids_list.append(item['id'])
+
+movie_counts = {}
+
+for name, person_id in zip(names_list, ids_list):
+    credits_url = f"https://api.themoviedb.org/3/person/{person_id}/movie_credits?api_key=tmdb.API_KEY&language=en-US"
+    credits_response = requests.get(credits_url, headers=headers)
+    credits_data = credits_response.json()
+    
+    #assums "cast" contains the movies they acted in idk 
+    movie_counts[name] = len(credits_data['cast'])
+
+
 
 print(names_list)
-
-for person in names_list:
+print(ids_list)
+print(movie_counts)
     
 
+
+
+#scatter plot
+names = list(movie_counts.keys())
+counts = list(movie_counts.values())
+
+# Creating the scatter plot
+plt.figure(figsize=(10, 8))
+plt.scatter(names, counts, color='blue')
+plt.title('Number of Movies per Actor')
+plt.xlabel('Actor')
+plt.ylabel('Number of Movies')
+plt.xticks(rotation=90)
+plt.show()
 
 
 '''
