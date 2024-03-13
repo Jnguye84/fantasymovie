e62a9427413783d985467bfd1ccb490ec96b7ea3
Tahir. I <69819070+tismailw@users.noreply.github.com>
2024-03-13 15:55:21 -0400

    Update tmbd.py
    Updated everything into different methods so it can be used in twitter_people.py

--- tmbd.py
+++ tmbd.py
@@ -1,6 +1,8 @@
 import tmdbsimple as tmdb
 import requests
 import matplotlib.pyplot as plt
+import tweepy
+
 
 #api key
 tmdb.API_KEY = 'e4194ed7bb70d235c3f504a4165912d3'
@@ -17,57 +19,53 @@ headers = {
 
 
 #getting a list of popular people 
-response = requests.get(url, headers=headers)
-data = response.json()
+def peopleList():
+    response = requests.get(url, headers=headers)
+    data = response.json()
 
-names_list = []
-ids_list = []
+    names_list = []
 
-# Iterate over the items in the 'results' list
-for item in data['results']:
-    # Extract the name of each person and append it to the names_list
-    names_list.append(item['name'])
-    ids_list.append(item['id'])
+    # Iterate over the items in the 'results' list
+    for item in data['results']:
+        # Extract the name of each person and append it to the names_list
+        name_lowercased = item['name'].lower()
+        names_list.append(name_lowercased)
+
+    return (names_list)
+
+
+#getting the ids of people
+def idList():
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    names_list = []
+    ids_list = []
+
+    # Iterate over the items in the 'results' list
+    for item in data['results']:
+        # Extract the name of each person and append it to the names_list
+        name_lowercased = item['name'].lower()
+        names_list.append(name_lowercased)
+
+        ids_list.append(item['id'])
+
+
+    return (ids_list)
+
 
 movie_counts = {}
 
-for name, person_id in zip(names_list, ids_list):
-    credits_url = f"https://api.themoviedb.org/3/person/{person_id}/movie_credits?api_key=tmdb.API_KEY&language=en-US"
-    credits_response = requests.get(credits_url, headers=headers)
-    credits_data = credits_response.json()
-    
-    #assums "cast" contains the movies they acted in idk 
-    movie_counts[name] = len(credits_data['cast'])
+# getting a dictionary of the 2 
 
+def peopleAndId():
+    movie_counts = {}
+    for name, person_id in zip(peopleList(), idList()):
+        credits_url = f"https://api.themoviedb.org/3/person/{person_id}/movie_credits?api_key=tmdb.API_KEY&language=en-US"
+        credits_response = requests.get(credits_url, headers=headers)
+        credits_data = credits_response.json()
+        
 
+        movie_counts[name] = len(credits_data['cast'])
 
-print(names_list)
-print(ids_list)
-print(movie_counts)
-    
-
-
-
-#scatter plot
-names = list(movie_counts.keys())
-counts = list(movie_counts.values())
-
-# Creating the scatter plot
-plt.figure(figsize=(10, 8))
-plt.scatter(names, counts, color='blue')
-plt.title('Number of Movies per Actor')
-plt.xlabel('Actor')
-plt.ylabel('Number of Movies')
-plt.xticks(rotation=90)
-plt.show()
-
-
-'''
-#movie = tmdb.Movies(205)
-search = tmdb.Search()
-response = search.person(query='Denzel Washington')
-response = search.popular()
-
-#response = movie.info()
-print(response)
-'''
+    return(movie_counts)
